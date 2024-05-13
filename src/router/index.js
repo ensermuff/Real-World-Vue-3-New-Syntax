7b0e982a13c932be10d0c9124de5fea441cc0c0a
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2024-05-12 22:57:24 -0400

    Working on Touring-Vue-Router Vue Mastery course

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventListView from '../views/EventListView.vue'
-import EventDetailsView from '../views/EventDetailsView.vue'
-import AboutView from '../views/AboutView.vue'
+import About from '../views/AboutView.vue'
+import EventList from '../views/EventListView.vue'
+import EventDetails from '../views/event/EventDetails.vue'
+import EventRegister from '../views/event/EventRegister.vue'
+import EventEdit from '../views/event/EventEdit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'event-list',
-      component: EventListView,
+      name: 'EventList',
+      component: EventList,
+      props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
     {
       path: '/event/:id',
-      name: 'event-details',
+      name: 'EventDetails',
       props: true,
-      component: EventDetailsView,
+      component: EventDetails,
+    },
+    {
+      path: '/event/:id/register',
+      name: 'EventRegister',
+      props: true,
+      component: EventRegister,
+    },
+    {
+      path: '/event/:id/edit',
+      name: 'EventEdit',
+      props: true,
+      component: EventEdit,
     },
     {
       path: '/about',
-      name: 'about',
-      component: AboutView,
+      name: 'About',
+      component: About,
     },
   ],
 })
